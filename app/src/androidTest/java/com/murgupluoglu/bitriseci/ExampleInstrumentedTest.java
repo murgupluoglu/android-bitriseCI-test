af6939075abc6cbcdd3a656b50a5e2094c4942b6
Mustafa Urgupluoglu <mustafa.urgupluoglu@sigmatelecom.com>
2018-05-31 10:31:59 +0300

    Instrumentedtest - fail

--- app/src/androidTest/java/com/murgupluoglu/bitriseci/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/murgupluoglu/bitriseci/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.murgupluoglu.bitriseci?", appContext.getPackageName());
+        assertEquals("com.murgupluoglu.bitriseci??", appContext.getPackageName());
     }
 }
